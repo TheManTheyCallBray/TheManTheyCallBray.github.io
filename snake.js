d4e937dac5d4e52c4c6768e68a69acda89d7d189
TheManTheyCallBray <100329874+TheManTheyCallBray@users.noreply.github.com>
2022-02-24 18:35:00 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -10,7 +10,7 @@ var BRAY_SN;
     var wrapper=document.createElement("div"),
     frame=document.createElement("iframe");
     frame.src="https://www.googlesnake.com/snakedesktop.php";
-    frame.width=713; // additional 30px to hide scrollbars
+    frame.width=683; // additional 30px to hide scrollbars
     frame.height=681;
     frame.scrolling="no";
     applyStyles(frame,{
